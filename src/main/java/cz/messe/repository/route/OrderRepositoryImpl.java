9cb70f6890702251e1222187d63f55ca213efca4
Jiri Grunwald <jirka.grunwald@roke.cz>
2018-03-05 01:39:35 +0100

    Add transaction to order repository

--- src/main/java/cz/messe/repository/route/OrderRepositoryImpl.java
+++ src/main/java/cz/messe/repository/route/OrderRepositoryImpl.java
@@ -4,10 +4,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import cz.messe.model.order.OrderState;
 
 @Repository
+@Transactional
 public class OrderRepositoryImpl implements OrderRepositoryCustom {
 
     @PersistenceContext
